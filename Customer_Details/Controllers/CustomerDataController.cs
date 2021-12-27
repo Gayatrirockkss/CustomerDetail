e9fee2eedb9c7a43b8c8a1ceca0c1a0799c2f66e
Gayatrirockss <gayatrishinde180898@gmail.com>
2021-12-27 20:02:32 +0530

    initial commit

---
+++ Customer_Details/Controllers/CustomerDataController.cs
@@ -0,0 +1,105 @@
+﻿using Customer_Details.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Customer_Details.Service;
+using System.Threading.Tasks;
+
+namespace Customer_Details.Controllers
+{
+    public class CustomerDataController : Controller
+    {
+        private Intern_DBEntities dbcontext = new Intern_DBEntities();
+        public bool status;
+        #region============ GET() Method================================
+        public async Task<ActionResult> Index()
+        {
+            return View(await CustomerDataAccess.GetDataAsync());
+        }
+         #endregion
+
+        #region=====================Create() Method================
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create([Bind(Include = "Person_ID,Person_Name,Person_Age,Person_Occupation,Person_Mail")] PersonalDataDetail personalDataDetail)
+        {
+            status = await CustomerDataAccess.AddDataAsync(personalDataDetail);
+            if (status == true)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("DataNotFound");
+            }
+        }
+        #endregion
+
+        #region================Update() Method==========================================
+
+        public async Task<ActionResult> Update(int? id)
+        {
+            if (id == null)
+            {
+                return View("DataNotFound");
+            }
+
+            return View(await CustomerDataAccess.UpdateDataAsync(id));
+        }
+        [HttpPost]
+        public async Task<ActionResult> Update([Bind(Include = "Person_ID,Person_Name,Person_Age,Person_Occupation,Person_Mail")] PersonalDataDetail personalDataDetail)
+        {
+            status = await CustomerDataAccess.UpdateDataAsync(personalDataDetail);
+            if(status == true)
+            {
+                return View();
+            }
+            else
+            {
+                return View("DataNotFound");
+            }
+        }
+        #endregion
+
+        #region======================Delete() Method========================================
+
+        
+        
+        public async Task<ActionResult> Delete(int id)
+        {
+            status = await CustomerDataAccess.DeleteDataAsync(id);
+            if (status == true)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("DataNotFound");
+            }
+            
+        }
+        #endregion
+
+        #region============================Detail() method=========================================
+
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return View("DataNotFound");
+            }
+            
+            return View(await CustomerDataAccess.GetDetailDataAsync(id));
+        }
+        #endregion
+
+    }
+}
---
+++ Customer_Details/Service/CustomerDataAccess.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using Customer_Details.Models;
+
+namespace Customer_Details.Service
+{
+    public class CustomerDataAccess
+    {
+        public async static Task<List<PersonalDataDetail>> GetDataAsync()
+        {
+            using(var dbcontext = new Intern_DBEntities())
+            {
+                var query = from item in dbcontext.PersonalDataDetails
+                            select item;
+                return await query.ToListAsync();
+            }
+        }
+
+        public async static Task<bool> AddDataAsync(PersonalDataDetail personalDataDetail)
+        {
+            if (personalDataDetail != null)
+            {
+                using (var dbcontext = new Intern_DBEntities())
+                {
+                    dbcontext.PersonalDataDetails.Add(personalDataDetail);
+                    await dbcontext.SaveChangesAsync();
+                    return true;
+
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public async static Task<bool> UpdateDataAsync(PersonalDataDetail personalDataDetail)
+        {
+            if (personalDataDetail != null)
+            {
+                using (var dbcontext = new Intern_DBEntities())
+                {
+                    dbcontext.PersonalDataDetails.Add(personalDataDetail);
+                    dbcontext.Entry(personalDataDetail).State = EntityState.Modified;
+                    await dbcontext.SaveChangesAsync();
+                    return true;
+
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public async static Task<PersonalDataDetail> UpdateDataAsync(int? id)
+        {
+            using (var dbcontext = new Intern_DBEntities())
+            {
+                var personalDataDetail = dbcontext.PersonalDataDetails.Where(s => s.Person_ID == id).FirstOrDefaultAsync();
+
+                return await personalDataDetail;
+
+            }
+        }
+
+        public async static Task<bool> DeleteDataAsync(int id)
+        {
+            using (var dbcontext = new Intern_DBEntities())
+            {
+                var personalDataDetail = dbcontext.PersonalDataDetails.Where(s => s.Person_ID == id).FirstOrDefault();
+                if (personalDataDetail != null)
+                {
+                    dbcontext.PersonalDataDetails.Remove(personalDataDetail);
+                    await dbcontext.SaveChangesAsync();
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+        }
+
+        public async static Task<PersonalDataDetail> GetDetailDataAsync(int? id)
+        {
+            using (var dbcontext = new Intern_DBEntities())
+            {
+                var personalDataDetail = dbcontext.PersonalDataDetails.Where(s => s.Person_ID == id).FirstOrDefaultAsync();
+                
+                return await personalDataDetail;
+
+            }
+        }
+        
+    }
+}
